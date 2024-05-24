5feadd01ec79722ec99f6fc0c4ea30a7f289d468
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-24 12:18:53 +0600

    Student interface is completed

---
+++ Backend/src/app/modules/student/student.interface.ts
@@ -0,0 +1,42 @@
+import { Model } from 'mongoose';
+
+export type TUserName = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type TGuardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type TLocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type TStudent = {
+  id: string;
+  password: string;
+  name: TUserName;
+  gender: 'male' | 'female';
+  dateOfBirth?: string;
+  email: string;
+  contactNumber: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'AB+' | 'AB-' | 'B+' | 'B-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: TGuardian;
+  localGuardian: TLocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'blocked';
+  isDeleted: boolean;
+};
