55064a3224d7b2cf6226f208408a3e2b4faed65b
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-24 12:33:20 +0600

    student model is created

---
+++ Backend/src/app/modules/student/student.model.ts
@@ -0,0 +1,153 @@
+import { Schema, model } from 'mongoose';
+import {
+  TGuardian,
+  TLocalGuardian,
+  TStudent,
+  TUserName,
+} from './student.interface';
+
+const userNameSchema = new Schema<TUserName>({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+    trim: true,
+    maxLength: [15, 'Maximum allowed length for first name is 15 character'],
+  },
+  middleName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: [true, 'Last name is required'],
+  },
+});
+
+const guardianSchema = new Schema<TGuardian>({
+  fatherName: {
+    type: String,
+    trim: true,
+    required: [true, "Father's name is required"],
+  },
+  fatherOccupation: {
+    type: String,
+    required: [true, "Father's occupation is required"],
+  },
+  fatherContactNo: {
+    type: String,
+    required: [true, "Father's contact number is required"],
+  },
+  motherName: {
+    type: String,
+    trim: true,
+    required: [true, "Mother's name is required"],
+  },
+  motherOccupation: {
+    type: String,
+    required: [true, "Mother's occupation is required"],
+  },
+  motherContactNo: {
+    type: String,
+    required: [true, "Mother's contact number is required"],
+  },
+});
+
+const localGuardianSchema = new Schema<TLocalGuardian>({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, "Local guardian's name is required"],
+  },
+  occupation: {
+    type: String,
+    required: [true, "Local guardian's occupation is required"],
+  },
+  contactNo: {
+    type: String,
+    required: [true, "Local guardian's contact number is required"],
+  },
+  address: {
+    type: String,
+    required: [true, "Local guardian's address is required"],
+  },
+});
+
+const studentSchema = new Schema<TStudent>({
+  id: {
+    type: String,
+    required: [true, 'Student ID is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    maxLength: [20, "Password can't be more then 20 character"],
+  },
+  name: {
+    type: userNameSchema,
+    trim: true,
+    required: [true, 'Name is required'],
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female'],
+      message: '{VALUE} is not supported. It must be either "male" or "female"',
+    },
+    required: [true, 'Gender is required'],
+  },
+  dateOfBirth: { type: String },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  contactNumber: {
+    type: String,
+    required: [true, 'Contact number is required'],
+  },
+  emergencyContactNo: {
+    type: String,
+    required: [true, 'Emergency contact number is required'],
+  },
+  bloodGroup: {
+    type: String,
+    enum: {
+      values: ['A+', 'A-', 'AB+', 'AB-', 'B+', 'B-', 'O+', 'O-'],
+      message: '{VALUE} is not a valid blood group',
+    },
+  },
+  presentAddress: {
+    type: String,
+    required: [true, 'Present address is required'],
+  },
+  permanentAddress: {
+    type: String,
+    required: [true, 'Permanent address is required'],
+  },
+  guardian: {
+    type: guardianSchema,
+    required: [true, 'Guardian information is required'],
+  },
+  localGuardian: {
+    type: localGuardianSchema,
+    required: [true, 'Local guardian information is required'],
+  },
+  profileImg: { type: String },
+  isActive: {
+    type: String,
+    enum: {
+      values: ['active', 'blocked'],
+      message:
+        '{VALUE} is not supported. It must be either "active" or "blocked"',
+    },
+    default: 'active',
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const Student = model<TStudent>('Student', studentSchema);
