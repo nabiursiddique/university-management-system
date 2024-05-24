e2dc94a7d0a8aad7f3f90c3e871c1c1335766f43
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-24 11:24:39 +0600

    Project setup is completed

---
+++ Backend/src/config/index.ts
@@ -0,0 +1,6 @@
+import "dotenv/config";
+
+export default {
+  port: process.env.PORT,
+  db_url: process.env.DB_URL,
+};
---
+++ Backend/src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import config from "./config";
+
+async function main() {
+  try {
+    await mongoose.connect(config.db_url as string);
+    const port = config.port;
+    app.listen(port, () => {
+      console.log(`university management app is listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
