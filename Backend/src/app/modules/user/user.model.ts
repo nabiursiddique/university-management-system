e785b7ebb821461c7576c13a2e076b8fda5143d0
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-25 16:54:42 +0600

    user interface and model is created

---
+++ Backend/src/app/modules/user/user.model.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ['admin', 'student', 'faculty'],
+      },
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ['in-progress', 'blocked'],
+      },
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = model<TUser>('User', userSchema);
