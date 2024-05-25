ba5fc788263d8d4fde40083b01bd5a2a40c16f72
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-26 00:51:45 +0600

    route is now organized

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,9 +1,8 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFoundRoute from './app/middlewares/notFoundRoute';
+import router from './app/routes';
 
 export const app: Application = express();
 
@@ -11,16 +10,15 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
-
-// server route
-app.get('/', (req: Request, res: Response) => {
-  res.send('welcome to university management system');
-});
+app.use('/api/v1', router);
 
 // not found route
 app.use(notFoundRoute);
 
 // error handling for whole project
 app.use(globalErrorHandler);
+
+// server route
+app.get('/', (req: Request, res: Response) => {
+  res.send('welcome to university management system');
+});
