bf82a3082a8be08ef38a423057d61f62a47f2ec6
nabiur siddique <nabiursiddique01@gmail.com>
2024-06-01 00:20:07 +0600

    Implemented transaction and rollback while creating user and student

--- Backend/src/app/modules/user/user.service.ts
+++ Backend/src/app/modules/user/user.service.ts
@@ -7,6 +7,7 @@ import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
 import { generateStudentId } from './user.utils';
+import mongoose from 'mongoose';
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
   //  Create a user object
@@ -23,23 +24,42 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     payload.admissionSemester,
   );
 
-  // set manually generated id
-  if (!admissionSemester) {
-    throw new AppError(httpStatus.NOT_FOUND, 'Admission semester not found');
-  }
-  userData.id = await generateStudentId(admissionSemester);
+  const session = await mongoose.startSession();
 
-  // Create a user
-  const newUser = await User.create(userData);
+  try {
+    session.startTransaction();
 
-  // create a student
-  if (Object.keys(newUser).length) {
+    // set generated id
+    if (!admissionSemester) {
+      throw new AppError(httpStatus.NOT_FOUND, 'Admission semester not found');
+    }
+    userData.id = await generateStudentId(admissionSemester);
+
+    // Create a user (transaction-01)
+    const newUser = await User.create([userData], { session });
+
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user');
+    }
+
     // set id, _id as user
-    payload.id = newUser.id;
-    payload.user = newUser._id; //reference _id
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id; //reference _id
 
-    const newStudent = await Student.create(payload);
+    // Create a student (transaction-02)
+    const newStudent = await Student.create([payload], { session });
+
+    if (!newStudent.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
     return newStudent;
+  } catch (err) {
+    await session.abortTransaction();
+    await session.endSession();
   }
 };
 
