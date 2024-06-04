0131c11c47dc509cdb5ef8903806e3c9c0e9c2aa
nabiur siddique <nabiursiddique01@gmail.com>
2024-06-04 23:52:25 +0600

    course interface and model is created

---
+++ Backend/src/app/modules/course/course.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose';
+import { TCourse, TPreRequisiteCourses } from './course.interface';
+
+const PreRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
+  course: {
+    type: Schema.Types.ObjectId,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const CourseSchema = new Schema<TCourse>({
+  title: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  },
+  prefix: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  code: {
+    type: Number,
+    trim: true,
+    required: true,
+  },
+  credits: {
+    type: Number,
+    trim: true,
+    required: true,
+  },
+  preRequisiteCourses: [PreRequisiteCoursesSchema],
+});
+
+export const Course = model<TCourse>('Course', CourseSchema);
