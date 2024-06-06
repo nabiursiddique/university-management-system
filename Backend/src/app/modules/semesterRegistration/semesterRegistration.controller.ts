f014e222f1e66f836a0b4b42b462c14807ba3edc
nabiur siddique <nabiursiddique01@gmail.com>
2024-06-06 14:18:16 +0600

    create semester registration is implemented

--- Backend/src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ Backend/src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -1,8 +1,23 @@
 import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
+import { SemesterRegistrationServices } from './semesterRegistration.service';
 
 const createSemesterRegistration = catchAsync(
-  async (req: Request, res: Response) => {},
+  async (req: Request, res: Response) => {
+    const result =
+      await SemesterRegistrationServices.createSemesterRegistrationIntoDB(
+        req.body,
+      );
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Semester Registration is created successfully',
+      data: result,
+    });
+  },
 );
 
 const getALlSemesterRegistrations = catchAsync(
--- Backend/src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ Backend/src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,4 +1,40 @@
-const createSemesterRegistrationIntoDB = async () => {};
+import httpStatus from 'http-status';
+import AppError from '../../errors/appError';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
+import { TSemesterRegistration } from './semesterRegistration.interface';
+import { SemesterRegistration } from './semesterRegistration.model';
+
+const createSemesterRegistrationIntoDB = async (
+  payload: TSemesterRegistration,
+) => {
+  const academicSemester = payload?.academicSemester;
+
+  // checking if the academic semester already exists
+  const isAcademicSemesterExists =
+    await AcademicSemester.findById(academicSemester);
+
+  if (!isAcademicSemesterExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'This academic semester not found!',
+    );
+  }
+
+  // check if the the semester is already registered
+  const isSemesterRegistrationExists = await SemesterRegistration.findOne({
+    academicSemester,
+  });
+
+  if (isSemesterRegistrationExists) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      'This semester is already registered',
+    );
+  }
+
+  const result = await SemesterRegistration.create(payload);
+  return result;
+};
 
 const getAllSemesterRegistrationFromDB = async () => {};
 
