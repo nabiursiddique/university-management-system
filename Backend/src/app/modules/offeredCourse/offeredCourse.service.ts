ff9fcf98b17796f4530d427cad6b38877e8949d0
nabiur siddique <nabiursiddique01@gmail.com>
2024-06-07 12:26:40 +0600

    time confilction for a faculty is handled

--- Backend/src/app/modules/offeredCourse/offeredCourse.service.ts
+++ Backend/src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -16,6 +16,9 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     course,
     section,
     faculty,
+    days,
+    startTime,
+    endTime,
   } = payload;
 
   // check if the semester registration id exists
@@ -81,8 +84,39 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     );
   }
 
+  // get the schedule of the faculties
+  const assignedSchedules = await OfferedCourse.find({
+    semesterRegistration,
+    faculty,
+    days: { $in: days },
+  }).select('days startTime endTime');
+
+  console.log(assignedSchedules);
+
+  const newSchedule = {
+    days,
+    startTime,
+    endTime,
+  };
+
+  assignedSchedules.forEach((schedule) => {
+    const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
+    const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
+    const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
+    const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
+
+    if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
+      throw new AppError(
+        httpStatus.CONFLICT,
+        `This faculty is not available at that time ! choose another time or day`,
+      );
+    }
+  });
+
   const result = await OfferedCourse.create({ ...payload, academicSemester });
   return result;
+
+  // return null;
 };
 
 export const OfferedCourseServices = {
