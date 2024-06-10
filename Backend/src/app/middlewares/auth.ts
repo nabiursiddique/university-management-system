916af22d23174a8139fb2c8229db52910bc09bb1
nabiur siddique <nabiursiddique01@gmail.com>
2024-06-10 13:01:12 +0600

    verifing token using auth

--- Backend/src/app/middlewares/auth.ts
+++ Backend/src/app/middlewares/auth.ts
@@ -2,14 +2,31 @@ import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/appError';
 import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // checking if the token is sent from the client
     const token = req.headers.authorization;
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'you are not authorized');
     }
-    next();
+
+    // checking if the token is valid
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        // err
+        if (err) {
+          throw new AppError(httpStatus.UNAUTHORIZED, 'you are not authorized');
+        }
+        // decoded
+        req.user = decoded as JwtPayload;
+        next();
+      },
+    );
   });
 };
 
