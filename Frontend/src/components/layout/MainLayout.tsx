08ba85546b8454dcaa7a0583b42127625a46c858
Nabiur siddique <nabiursiddique01@gmail.com>
2024-07-01 00:02:27 +0600

    fixed the layout and did some little modification

--- Frontend/src/components/layout/MainLayout.tsx
+++ Frontend/src/components/layout/MainLayout.tsx
@@ -1,26 +1,34 @@
-import { Layout, Menu } from "antd";
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { createElement } from "react";
+import { Layout, Menu, MenuProps } from "antd";
 const { Header, Content, Footer, Sider } = Layout;
 
-const items = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  UserOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: createElement(icon),
-  label: `nav ${index + 1}`,
-}));
+const items: MenuProps["items"] = [
+  {
+    key: "fadsf",
+    label: "Dashboard",
+  },
+  {
+    key: "fasdf",
+    label: "Profile",
+  },
+  {
+    key: "fassa",
+    label: "User Management",
+    children: [
+      {
+        key: "111",
+        label: "Create Student",
+      },
+      {
+        key: "112",
+        label: "Create Faculty",
+      },
+    ],
+  },
+];
 
 const MainLayout = () => {
   return (
-    <Layout>
+    <Layout style={{ height: "100vh" }}>
       <Sider
         breakpoint="lg"
         collapsedWidth="0"
@@ -31,7 +39,17 @@ const MainLayout = () => {
           console.log(collapsed, type);
         }}
       >
-        <div className="demo-logo-vertical" />
+        <div
+          style={{
+            color: "white",
+            height: "4rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>PH Uni</h1>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
