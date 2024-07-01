071dbe8a99b037f54800a5a719c710cd2ec5f703
Nabiur siddique <nabiursiddique01@gmail.com>
2024-07-01 13:32:18 +0600

    react router setup is ongoing

--- Frontend/src/components/layout/MainLayout.tsx
+++ Frontend/src/components/layout/MainLayout.tsx
@@ -1,4 +1,5 @@
 import { Layout, Menu, MenuProps } from "antd";
+import { Outlet } from "react-router-dom";
 const { Header, Content, Footer, Sider } = Layout;
 
 const items: MenuProps["items"] = [
@@ -66,7 +67,7 @@ const MainLayout = () => {
               minHeight: 360,
             }}
           >
-            The main content should go here
+            <Outlet />
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
