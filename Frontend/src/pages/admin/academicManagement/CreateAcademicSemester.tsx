48f49b06f545ef42c4c88bd9430b04814d6869f0
Nabiur siddique <nabiursiddique01@gmail.com>
2024-07-15 13:36:29 +0600

    type error are handled

--- Frontend/src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ Frontend/src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -8,6 +8,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { academicSemesterSchema } from '../../../schemas/academicManagement.schema';
 import { useAddAcademicSemesterMutation } from '../../../redux/features/admin/academicManagement.api';
 import { toast } from 'sonner';
+import { TResponse } from '../../../types/global';
 
 const currentYear = new Date().getFullYear();
 const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
@@ -19,6 +20,8 @@ const CreateAcademicSemester = () => {
   const [addAcademicSemester] = useAddAcademicSemesterMutation();
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading('Creating.....');
+
     const name = semesterOptions[Number(data?.name) - 1]?.label;
 
     const semesterData = {
@@ -30,10 +33,14 @@ const CreateAcademicSemester = () => {
     };
 
     try {
-      const res = await addAcademicSemester(semesterData);
-      console.log(res);
+      const res = (await addAcademicSemester(semesterData)) as TResponse;
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success('Semester Created Successfully', { id: toastId });
+      }
     } catch (err) {
-      toast.error('Something went wrong');
+      toast.error('Something went wrong', { id: toastId });
     }
   };
 
--- Frontend/src/redux/api/baseApi.ts
+++ Frontend/src/redux/api/baseApi.ts
@@ -5,19 +5,19 @@ import {
   DefinitionType,
   FetchArgs,
   fetchBaseQuery,
-} from "@reduxjs/toolkit/query/react";
-import { RootState } from "../store";
-import { logout, setUser } from "../features/auth/authSlice";
-import { toast } from "sonner";
+} from '@reduxjs/toolkit/query/react';
+import { RootState } from '../store';
+import { logout, setUser } from '../features/auth/authSlice';
+import { toast } from 'sonner';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api/v1",
-  credentials: "include",
+  baseUrl: 'http://localhost:5000/api/v1',
+  credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
 
     if (token) {
-      headers.set("authorization", `${token}`);
+      headers.set('authorization', `${token}`);
     }
 
     return headers;
@@ -32,16 +32,16 @@ const baseQueryWithRefreshToken: BaseQueryFn<
   let result = await baseQuery(args, api, extraOptions);
 
   if (result?.error?.status === 404) {
-    toast.error("User not fount");
+    toast.error(result.error.data.message);
   }
 
   if (result?.error?.status === 401) {
     //* send refresh token
-    console.log("sending refresh token");
+    console.log('sending refresh token');
 
-    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
-      method: "POST",
-      credentials: "include",
+    const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
+      method: 'POST',
+      credentials: 'include',
     });
 
     const data = await res.json();
@@ -65,7 +65,7 @@ const baseQueryWithRefreshToken: BaseQueryFn<
 };
 
 export const baseApi = createApi({
-  reducerPath: "baseApi",
+  reducerPath: 'baseApi',
   baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
