67544492e11711065df42da256c725797e1c1742
Nabiur siddique <nabiursiddique01@gmail.com>
2024-07-30 12:12:25 +0600

    table is created

---
+++ Frontend/src/pages/admin/userManagement/StudentData.tsx
@@ -0,0 +1,78 @@
+import { Button, Space, Table } from 'antd';
+import type { TableColumnsType, TableProps } from 'antd';
+import { useState } from 'react';
+import { TQueryParam, TStudent } from '../../../types';
+import { useGetAllStudentsQuery } from '../../../redux/features/admin/userManagement.api';
+
+export type TTableData = Pick<TStudent, 'name' | 'id'>;
+
+const StudentData = () => {
+  const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+  const { data: studentData, isFetching } = useGetAllStudentsQuery(params);
+
+  const tableData = studentData?.data?.map(({ _id, fullName, id }) => ({
+    key: _id,
+    _id,
+    fullName,
+    id,
+  }));
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: 'Name',
+      key: 'name',
+      dataIndex: 'fullName',
+    },
+    {
+      title: 'Roll No',
+      key: 'id',
+      dataIndex: 'id',
+    },
+    {
+      title: 'Action',
+      key: 'x',
+      render: () => {
+        return (
+          <Space>
+            <Button>Details</Button>
+            <Button>Update</Button>
+            <Button>Block</Button>
+          </Space>
+        );
+      },
+      width: '1%',
+    },
+  ];
+
+  const onChange: TableProps<TTableData>['onChange'] = (
+    _pagination,
+    filters,
+    _sorter,
+    extra
+  ) => {
+    if (extra.action === 'filter') {
+      const queryParams: TQueryParam[] = [];
+      filters.name?.forEach((item) =>
+        queryParams.push({ name: 'name', value: item })
+      );
+      filters.year?.forEach((item) =>
+        queryParams.push({ name: 'year', value: item })
+      );
+      setParams(queryParams);
+    }
+  };
+
+  return (
+    <div>
+      <Table
+        loading={isFetching}
+        columns={columns}
+        dataSource={tableData}
+        onChange={onChange}
+        showSorterTooltip={{ target: 'sorter-icon' }}
+      />
+    </div>
+  );
+};
+
+export default StudentData;
