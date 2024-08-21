2e607c1f679fb288cfe043cc8492f28580bf213c
Nabiur siddique <nabiursiddique01@gmail.com>
2024-08-21 23:38:55 +0600

    Now we can get the offered course data

--- Frontend/src/pages/student/OfferedCourse.tsx
+++ Frontend/src/pages/student/OfferedCourse.tsx
@@ -1,4 +1,8 @@
+import { useGetAllOfferedCoursesQuery } from '../../redux/features/student/studentCourseManagement.api';
+
 const OfferedCourse = () => {
+  const { data } = useGetAllOfferedCoursesQuery(undefined);
+  console.log(data);
   return (
     <div>
       <h1>This is offered course component</h1>
