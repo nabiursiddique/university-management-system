26213d5b3c8bf7a6fc728bbced5f39c30ccc1d59
Nabiur siddique <nabiursiddique01@gmail.com>
2024-08-22 11:32:32 +0600

    enrolled course functionality is implemented

---
+++ Backend/src/app/modules/enrolledCourse/enrolledCourse.route.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { EnrolledCourseControllers } from './enrolledCourse.controller';
+import { USER_ROLE } from '../user/user.constant';
+import { EnrolledCourseValidations } from './enrolledCourse.validation';
+
+const router = express.Router();
+
+router.post(
+  '/create-enrolled-course',
+  auth(USER_ROLE.student),
+  validateRequest(
+    EnrolledCourseValidations.createEnrolledCourseValidationZodSchema,
+  ),
+  EnrolledCourseControllers.createEnrolledCourse,
+);
+
+router.get(
+  '/',
+  auth(USER_ROLE.faculty),
+  EnrolledCourseControllers.getAllEnrolledCourses,
+);
+
+router.get(
+  '/my-enrolled-courses',
+  auth(USER_ROLE.student),
+  EnrolledCourseControllers.getMyEnrolledCourses,
+);
+
+router.patch(
+  '/update-enrolled-course-marks',
+  auth(USER_ROLE.admin, USER_ROLE.faculty),
+  validateRequest(
+    EnrolledCourseValidations.updateEnrolledCourseMarksValidationZodSchema,
+  ),
+  EnrolledCourseControllers.updateEnrolledCourseMarks,
+);
+
+export const EnrolledCourseRoutes = router;
